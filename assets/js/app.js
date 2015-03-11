ffa8902baa5ef58cfeadce9a55988f767208dce6
Joe Denny <joe@learningconnexions.com>
2015-03-11 15:58:24 +0000

    everything done. just need to clean up

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,21 +32,4 @@ var stretch_button = function() {
 	}
 }
 
-// var stretch_button = function() {
-
-// 	var button_width = $('.btn').width();
-// 	var new_button_width = button_width * 1.3;
-
-// 	$('.btn').mouseenter(function() {
-// 		$(this).animate({
-// 			width: new_button_width
-// 		}, 400);
-// 	});
-// 	$('.btn').mouseleave(function() {
-// 		$(this).animate({
-// 			width: button_width
-// 		}, 400);
-// 	});	
-// }
-
 $(document).ready(main);
